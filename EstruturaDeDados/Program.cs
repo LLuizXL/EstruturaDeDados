af407675fcb85602d797fd310a9d0df7310001f8
LLuizXL <luiz.mwlo76@gmail.com>
2024-08-21 10:38:45 -0300

    trabalhando com branch novamente

--- EstruturaDeDados/Program.cs
+++ EstruturaDeDados/Program.cs
@@ -3,5 +3,7 @@
     private static void Main(string[] args)
     {
         Console.WriteLine("Criando minha primeira Branch!");
+        Console.WriteLine("Trabalhando com Git/Github");
+        Console.WriteLine("Mais uma tentativa de trabalhar com Git");
     }
 }
