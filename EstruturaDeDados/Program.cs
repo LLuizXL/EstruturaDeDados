bca17f208d9a8c3bbfc00a3d5f93b9d4b01dbc3c
LLuizXL <luiz.mwlo76@gmail.com>
2024-08-21 14:54:08 -0300

    Anotações sobre List + exemplos

--- EstruturaDeDados/Program.cs
+++ EstruturaDeDados/Program.cs
@@ -1,7 +1,90 @@
-﻿internal class Program
+﻿using System.Security.Cryptography.X509Certificates;
+
+internal class Program
 {
+
+    private static void imprimirLista(List<string> listas)
+    {
+        /*      Imprimindo Novamente                */
+
+        Console.WriteLine(); //Pular linha
+
+        Console.WriteLine("Minha Lista de Frutas: ");
+        foreach (string lista in listas)
+        {
+            Console.WriteLine(lista);
+        }
+    }
+
     private static void Main(string[] args)
     {
-        Console.WriteLine("Criando minha primeira Branch!");
+        // TRABALHANDO COM LISTA ( LIST )
+
+        //Criando uma lista
+        List<string> frutas = new List<string>();
+
+        /* Adicionar itens na Lista */
+        frutas.Add("Morango");
+        frutas.Add("Manga");
+        frutas.Add("Goiaba");
+        frutas.Add("Uva");
+
+        /* Imprimir os Itens da Lista */
+
+        imprimirLista(frutas);
+
+        /* imprimir elemento na posição específica */
+        Console.WriteLine("Fruta no índice 2: " + frutas[2]);
+
+        /* Inserir elemento em índice especifico */
+
+        frutas.Insert(2, "Maracujá");
+
+
+        /*      Imprimindo Novamente                */
+
+        imprimirLista(frutas);
+
+        /*     Alterar elemento de um Índice      */
+        frutas[4] = "Melancia";
+
+
+        /*      Imprimindo Novamente                */
+
+        imprimirLista(frutas);
+
+        /*    Removendo um elemento   da lista por índice específico  */
+        frutas.RemoveAt(2);
+
+
+        /*      Imprimindo Novamente                */
+        Console.WriteLine(); //Pular linha
+
+        Console.WriteLine("Minha Lista de Frutas: ");
+        foreach (string fruta in frutas)
+        {
+            Console.WriteLine(fruta);
+        }
+
+
+        /* Remover elementos pelo valor do elemento/conteúdo */
+        frutas.Remove("Morango");
+
+
+        /*      Imprimindo Novamente                */
+        imprimirLista(frutas);
+
+
+        /* Apagar todos os elementos da lista */
+        frutas.Clear();
+
+
+
+        imprimirLista(frutas);
+
+
+
+
     }
+
 }
